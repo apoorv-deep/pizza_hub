a3a527951a7cac4381a7049a74d64c4dd5da108f
Apoorv Deep <apoorvdeeps@gmail.com>
2024-08-04 00:06:18 +0530

    footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,10 +3,17 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="h-12 md:h-24 p-4 lg:px-20 xl:px-40 text-purple-900 flex items-center justify-between">
-      <Link href="/" className="font-bold text-xl">PIZZA HUB</Link>
-      <p>2024 © ALL RIGHTS RESERVED.</p>
-    </div>
+    <footer className="text-purple-500 text-white p-4 mt-8 flex flex-col items-center sm:flex-row sm:justify-between">
+      <div>
+        &copy; 2024 - ALL RIGHTS RESERVED | 
+        <strong> <Link href="/" >PIZZA HUB</Link> </strong>
+      </div>
+      <div className="mt-2 sm:mt-0"> 
+      <strong> Developed by </strong>
+        <a href="https://www.linkedin.com/in/apoorv-deep/" target="_blank" className="hover:underline"> Apoorv Deep</a> | 
+        <a href="mailto:apoorvdeeps@gmail.com" className="hover:underline"> Contact</a>
+      </div>
+    </footer>
   );
 };
 
